c7037f45f5d886e6a83c620901358a9b15bfa8fc
Shridhar Sarraf <shridharsarraf998@gmail.com>
2025-02-25 12:26:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const [showQR, setShowQR] = useState(false);
   const [data, setData] = useState('No result');
   const [arr, setArr] = useState([]);
-  const [pureRate, setPureRate ] = useState(91096.272);
+  const [pureRate, setPureRate ] = useState(92052.98);
   const [password, setPassword] = useState('');
   const [payloadRate, setPayloadRate] = useState();
   const [showModal, setShowModal] = useState(false);
